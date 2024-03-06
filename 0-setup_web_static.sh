e3c69d50712564ec8e36e115f8ea4be5eae4e43d
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-03-06 03:51:51 -0800

    setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#sets up web servers for the deployment of web_static
+sudo apt update -y
+sudo apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
+sudo sed -i '47 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
